a56423c149f4001c4199e2c3f2a2e6681137a82f
Rayson Murray <raysonmurray19009@gmail.com>
2026-01-07 20:34:10 +0530

    bug-fix

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,12 +1,9 @@
 import mysql from "mysql2/promise";
 
-export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
-  waitForConnections: true,
-  connectionLimit: 5,
-  connectTimeout: 10000,
-});
+export const db = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+    });
